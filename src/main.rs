7580a998f21d38dc975a16a747db4a91b50c5f30
dj4t9n <pandeyjatin47@gmail.com>
2021-05-04 23:31:37 +0530

    added screen ----TODO---- add flappy bird ascii art

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+
 fn main() {
-    println!("Hello, world!");
+    let screen_path = Path::new("./screen.txt");
+    let display = screen_path.display();
+
+    let mut file = match File::open(&screen_path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("{} contains:\n{}", display, s),
+    }
 }
